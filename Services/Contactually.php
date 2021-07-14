7c20618729c9ac78d0f3e5d8e0a60b43cd6ae0eb
Joel <joel.abudu@devit-team.com>
2021-07-14 08:20:57 +0000

    fleshing out some ugly api calls;

--- Services/Contactually.php
+++ Services/Contactually.php
@@ -12,19 +12,60 @@ class Services_Contactually
 {
     protected $connection = null;
     protected $auth_url   = 'https:
+    protected $cookie_path = '';
+    protected $sub_resources = array();
     public function __construct($params)
     {
+        $this->cookie_path = getcwd() . '/cookie.txt';
+        $this->sub_resources = array(
+            'accounts',
+            'buckets',
+            'contact_histories',
+            'contacts',
+            'followups',
+            'notes',
+            'tasks',
+            'users'
+        );
         $fields = '';
         foreach($params as $param => $value) {
             $fields .= "&user[$param]=".urlencode($value);
         }
         $connection = curl_init();
+        curl_setopt($connection, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt($connection,CURLOPT_URL, $this->auth_url);
         curl_setopt($connection,CURLOPT_POST, count($params));
-curl_setopt($connection,CURLOPT_HEADER, true);
-curl_setopt($connection, CURLOPT_VERBOSE, true);
+curl_setopt($connection, CURLOPT_COOKIEJAR, $this->cookie_path);
+curl_setopt($connection, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($connection,CURLOPT_POSTFIELDS, $fields);
-        $result = curl_exec($connection);
-print_r($result);
+        $response = curl_exec($connection);
+        curl_close($connection);
+    }
+    public function getContacts($limit = 10)
+    {
+        $contacts_url = 'https:
+        $contacts_url .= '?limit=' . (int) $limit;
+        $connection = curl_init();
+        curl_setopt($connection, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($connection,CURLOPT_URL, $contacts_url);
+        curl_setopt($connection, CURLOPT_COOKIEFILE, $this->cookie_path); 
+curl_setopt($connection, CURLOPT_SSL_VERIFYPEER, false);
+        $response = curl_exec($connection);
+$status = curl_getinfo($connection, CURLINFO_HTTP_CODE);
+echo "x- $status -x \n";
+        return json_decode($response);
+    }
+    public function getAccounts()
+    {
+        $accounts_url = 'https:
+        $connection = curl_init();
+        curl_setopt($connection, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($connection,CURLOPT_URL, $accounts_url);
+        curl_setopt($connection, CURLOPT_COOKIEFILE, $this->cookie_path); 
+curl_setopt($connection, CURLOPT_SSL_VERIFYPEER, false);
+        $response = curl_exec($connection);
+$status = curl_getinfo($connection, CURLINFO_HTTP_CODE);
+echo "x- $status -x \n";
+        return json_decode($response);
     }
 }
--- demo.php
+++ demo.php
@@ -1,6 +1,10 @@
 <?php
 include_once 'creds.php';
 include_once 'Services/Contactually.php';
+echo '<pre>';
 $service = new Services_Contactually(
             array('email' => $email, 'password' => $password)
         );
+print_r($service);
+$contacts = $service->getContacts();
+print_r($contacts);
