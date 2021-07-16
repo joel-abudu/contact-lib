22ff9fd826808d1453b5788de4f557cfed1595bb
Joel <joel.abudu@devit-team.com>
2021-07-16 04:49:37 +0000

    cleaned up the curl processing to only happen in one place;

--- Services/Contactually.php
+++ Services/Contactually.php
@@ -11,7 +11,6 @@ spl_autoload_register('Services_Contactually_autoload');
 class Services_Contactually
 {
     protected $connection = null;
-    protected $auth_url   = 'https:
     protected $cookie_path = '';
     protected $sub_resources = array();
     public function __construct($params)
@@ -27,45 +26,40 @@ class Services_Contactually
             'tasks',
             'users'
         );
+        foreach($params as $param => $value) {
+            unset($params[$param]);
+            $params["user[$param]"] = $value;
+        }
+        $auth_url = 'https:
+        $this->execute($auth_url, $params);
+    }
+    public function execute($uri, $params = array())
+    {
         $fields = '';
         foreach($params as $param => $value) {
             $fields .= "&user[$param]=".urlencode($value);
         }
         $connection = curl_init();
         curl_setopt($connection, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($connection,CURLOPT_URL, $this->auth_url);
+        curl_setopt($connection,CURLOPT_URL, $uri);
         curl_setopt($connection,CURLOPT_POST, count($params));
-curl_setopt($connection, CURLOPT_COOKIEJAR, $this->cookie_path);
-curl_setopt($connection, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($connection,CURLOPT_POSTFIELDS, $fields);
-        $response = curl_exec($connection);
-        curl_close($connection);
-    }
-    public function getContacts($limit = 10)
-    {
-        $contacts_url = 'https:
-        $contacts_url .= '?limit=' . (int) $limit;
-        $connection = curl_init();
-        curl_setopt($connection, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($connection,CURLOPT_URL, $contacts_url);
+        curl_setopt($connection, CURLOPT_COOKIEJAR, $this->cookie_path);
         curl_setopt($connection, CURLOPT_COOKIEFILE, $this->cookie_path); 
 curl_setopt($connection, CURLOPT_SSL_VERIFYPEER, false);
         $response = curl_exec($connection);
-$status = curl_getinfo($connection, CURLINFO_HTTP_CODE);
-echo "x- $status -x \n";
-        return json_decode($response);
+        $status = curl_getinfo($connection, CURLINFO_HTTP_CODE);
+        curl_close($connection);
+        return json_decode($response);        
+    }
+    public function getContacts($limit = 10)
+    {
+        $contacts_uri = 'https:
+        $contacts_uri .= '?limit=' . (int) $limit;
+        return $this->execute($contacts_uri);
     }
     public function getAccounts()
     {
         $accounts_url = 'https:
-        $connection = curl_init();
-        curl_setopt($connection, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($connection,CURLOPT_URL, $accounts_url);
-        curl_setopt($connection, CURLOPT_COOKIEFILE, $this->cookie_path); 
-curl_setopt($connection, CURLOPT_SSL_VERIFYPEER, false);
-        $response = curl_exec($connection);
-$status = curl_getinfo($connection, CURLINFO_HTTP_CODE);
-echo "x- $status -x \n";
-        return json_decode($response);
+        return $this->execute($accounts_url);
     }
 }
