41791000fbfbd10db123437d69296cfac2ab12e5
Joel <joel.abudu@devit-team.com>
2021-09-25 03:30:45 +0000

    implemented Note->create()

--- Services/Contactually/Note.php
+++ Services/Contactually/Note.php
@@ -7,4 +7,14 @@ class Services_Contactually_Note extends Services_Contactually_Base
     public $parent_contact_id = '';
     public $timestamp = '';
     protected $_show_uri = 'https:
+    protected $_create_uri = 'https:
+    public function create(array $params)
+    {
+        $note = array();
+        foreach($params as $key => $value) {
+            $note["note[$key]"] = $value;
+        }
+        $this->client->post($this->_create_uri, $note);
+        return (201 == $this->client->status) ? true : false;
+    }
 }
