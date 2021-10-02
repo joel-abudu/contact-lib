5f0533120d4473ee095f29fda9866303c26e3b3a
Joel <joel.abudu@devit-team.com>
2021-10-02 15:40:12 +0000

    implemented Contact->bucket() to assign contacts

--- Services/Contactually/Contact.php
+++ Services/Contactually/Contact.php
@@ -17,8 +17,16 @@ class Services_Contactually_Contact extends Services_Contactually_Base
     protected $_show_uri  = 'https:
     protected $_create_uri = 'https:
     protected $_delete_uri = 'https:
+    protected $_bucket_uri = 'https:
     public function create(array $params)
     {
         throw new Services_Contactually_Exception_NotImplemented("This method is not implemented");
+    }
+    public function bucket($contact_id, $bucket_id)
+    {
+        $this->bucket = str_replace('<id>', $contact_id, $this->_bucket_uri);
+        $params = array('id' => $contact_id, 'bucket_id' => $bucket_id);
+        $this->client->post($this->bucket, $params);
+        return (200 == $this->client->status) ? true : false;
     }
 }
