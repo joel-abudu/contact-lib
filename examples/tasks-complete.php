3359d49f72f13182a38be9c4422555765310de13
Joel <joel.abudu@devit-team.com>
2021-10-03 20:48:01 +0000

    implemented Task->complete()

---
+++ examples/tasks-complete.php
@@ -0,0 +1,7 @@
+<?php
+include_once '../creds.php';
+include_once '../Services/Contactually.php';
+$client = new Services_Contactually(array('apikey' => $apiKey));
+$task = new Services_Contactually_Task($client);
+$result = $task->complete(5153368);
+print_r($result);
