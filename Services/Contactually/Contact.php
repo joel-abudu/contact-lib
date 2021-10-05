ce485a5e79fa6e87ee8fda85352132cf8db480ab
Joel <joel.abudu@devit-team.com>
2021-10-05 04:59:54 +0000

    implemented Contact->ignore

--- Services/Contactually/Contact.php
+++ Services/Contactually/Contact.php
@@ -18,6 +18,7 @@ class Services_Contactually_Contact extends Services_Contactually_Base
     protected $_create_uri = 'https:
     protected $_delete_uri = 'https:
     protected $_bucket_uri = 'https:
+    protected $_ignore_uri = 'https:
     public function create(array $params)
     {
         throw new Services_Contactually_Exception_NotImplemented("This method is not implemented");
@@ -27,6 +28,13 @@ class Services_Contactually_Contact extends Services_Contactually_Base
         $this->bucket = str_replace('<id>', $contact_id, $this->_bucket_uri);
         $params = array('id' => $contact_id, 'bucket_id' => $bucket_id);
         $this->client->post($this->bucket, $params);
+        return (200 == $this->client->status) ? true : false;
+    }
+    public function ignore($contact_id, $temporary = true, $task_id = 0)
+    {
+        $this->ignore = str_replace('<id>', $contact_id, $this->_ignore_uri);
+        $params = array('id' => $contact_id, 'temp' => !$temporary, 'task_id' => $task_id);
+        $this->client->post($this->ignore, $params);
         return (200 == $this->client->status) ? true : false;
     }
 }
