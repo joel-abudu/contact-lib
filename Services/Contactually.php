7bf3b81e5412dda4906ce883df5a90da972ce880
Joel <joel.abudu@devit-team.com>
2021-10-09 15:11:28 +0000

    refactoring the core to get rid of some of the unnecessary complexity;

--- Services/Contactually.php
+++ Services/Contactually.php
@@ -56,4 +56,51 @@ class Services_Contactually extends Services_Contactually_Resources_Base
                     "Authentication failed");
         }
     }
+    public function get($uri, $params = array())
+    {
+        $uri .= (count($params)) ? '?'.http_build_query($params) : '';
+        $curl_opts = array(
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_URL => $uri,
+            CURLOPT_COOKIEFILE => $this->cookie_path,
+        );
+        return $this->_execute($curl_opts);
+    }
+    public function delete($uri, $params = array())
+    {
+        $curl_opts = array(
+            CURLOPT_CUSTOMREQUEST => 'DELETE',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_URL => $uri,
+            CURLOPT_POST => count($params),
+            CURLOPT_POSTFIELDS => $params,
+            CURLOPT_COOKIEJAR => $this->cookie_path,
+            CURLOPT_COOKIEFILE => $this->cookie_path, 
+        );
+        return $this->_execute($curl_opts);
+    }
+    public function post($uri, $params = array())
+    {
+        $curl_opts = array(
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_URL => $uri,
+            CURLOPT_POST => count($params),
+            CURLOPT_POSTFIELDS => $params,
+            CURLOPT_COOKIEJAR => $this->cookie_path,
+            CURLOPT_COOKIEFILE => $this->cookie_path, 
+        );
+        return $this->_execute($curl_opts);
+    }
+    protected function _execute($curl_params = array())
+    {
+        $connection = curl_init();
+        foreach($curl_params as $option => $value) {
+            curl_setopt($connection, $option, $value);
+        }
+curl_setopt($connection, CURLOPT_SSL_VERIFYPEER, false);
+        $this->response_body = curl_exec($connection);
+        $this->response_code = curl_getinfo($connection, CURLINFO_HTTP_CODE);
+        $this->response_json = json_decode($this->response_body);
+        curl_close($connection);
+    }
 }
