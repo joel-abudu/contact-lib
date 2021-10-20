4cb1380dd484d383f96ddbdefd7bc9054b263241
Joel <joel.abudu@devit-team.com>
2021-10-20 10:37:56 +0000

    fixing search to reflect the new http client structure

--- Services/Contactually/Contacts.php
+++ Services/Contactually/Contacts.php
@@ -9,9 +9,9 @@ class Services_Contactually_Contacts extends Services_Contactually_Resources_Lis
     public function search($term, $myArray = array())
     {
         $myArray['term'] = $term;
-        $json = $this->client->get($this->_search_uri, $myArray);
-        $object = json_decode($json);
-        $this->_json = $json;
+        $this->client->get($this->_search_uri, $myArray);
+        $object = $this->client->response_obj;
+        $this->_json = $this->client->response_json;
         $this->_obj  = $object->{$this->_data};
         $this->count = $object->count;
         return $this;
