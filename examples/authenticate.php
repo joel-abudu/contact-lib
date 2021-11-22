ad7374b0a89413b9b9ada54d095dc854efcde123
Joel <joel.abudu@devit-team.com>
2021-11-22 22:16:45 +0000

    deprecated the username/password method because it's a screwy cookie-based system that makes things more complicated than necessary

--- examples/authenticate.php
+++ examples/authenticate.php
@@ -1,12 +1,6 @@
 <?php
 include_once '../creds.php';
 include_once '../Services/Contactually.php';
-$client = new Services_Contactually(
-            array('email' => $email, 'password' => $password)
-        );
-echo $client->response_code;
-echo "\n";
-unset($client);
 $client = new Services_Contactually(array('api_key' => $apiKey));
 $buckets = $client->buckets->index();
 foreach($buckets as $bucket) {
