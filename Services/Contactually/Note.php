c5efa222538a247b7b3709ee6f63ea063e62121e
Joel <joel.abudu@devit-team.com>
2021-12-09 05:42:26 +0000

    updated the Note->create() method

--- Services/Contactually/Note.php
+++ Services/Contactually/Note.php
@@ -10,4 +10,12 @@ class Services_Contactually_Note extends Services_Contactually_Resources_Base
     protected $_show_uri   = 'notes/<id>.json';
     protected $_create_uri = 'notes.json';
     protected $_delete_uri = 'notes/<id>.json';
+    public function create(array $params)
+    {
+        $result = parent::create($params);
+        $payload = $this->_client->response_headers[2];
+        $data = json_decode($payload);
+        $result->location = 'https:
+        return $result;
+    }
 }
