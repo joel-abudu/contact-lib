f6e0a867c0ea31162e74fdfbb360227553b2cc49
Joel <joel.abudu@devit-team.com>
2021-12-14 09:23:28 +0000

    since the Location is missing in some cases, this checks for it

--- Services/Contactually.php
+++ Services/Contactually.php
@@ -113,7 +113,7 @@ curl_setopt($connection, CURLOPT_SSL_VERIFYPEER, false);
         {
             case '201':
                 preg_match("/Location:\s\S+/", $headers[1], $matches);
-                $location = substr($matches[0], strpos($matches[0], 'http'));
+                $location = (count($matches)) ? substr($matches[0], strpos($matches[0], 'http')) : '';
                 $response = '{"location": "' . $location . '", "status": "201"}';
                 break;
             case '406':
