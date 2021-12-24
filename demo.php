9757e0c88bde4d9510f3c0fbac2c3d8859991f6d
Joel <joel.abudu@devit-team.com>
2021-12-24 21:56:49 +0000

    tweak to the authentication

--- demo.php
+++ demo.php
@@ -2,8 +2,6 @@
 include_once 'creds.php';
 include_once 'Services/Contactually.php';
 echo '<pre>';
-$service = new Services_Contactually(
-            array('email' => $email, 'password' => $password)
-        );
+$service = new Services_Contactually(array('api_key' => $apiKey));
 $results = $service->contacts(array('limit' => 10));
 print_r($results);
