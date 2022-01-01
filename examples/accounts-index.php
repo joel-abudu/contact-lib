89b04ba74fd4a0a25bd93f35e22fd96b27476248
Joel <joel.abudu@devit-team.com>
2022-01-01 00:43:34 +0000

    reworking the account list using the basic client

--- examples/accounts-index.php
+++ examples/accounts-index.php
@@ -1,19 +1,10 @@
 <?php
 include_once '../creds.php';
-include_once '../Services/Contactually.php';
-$client = new Services_Contactually(array('api_key' => $apiKey));
-$accounts = $client->accounts->index();
-echo "\nDisplaying {$accounts->count} records per page:\n";
-echo "\nPage: " . $accounts->page . ' of ' . $accounts->getPageCount() . "\n";
-foreach($accounts as $account) {
-    echo $account->username . "\n";
-    echo $account->type . "\n";
-}
-while ($accounts->hasMorePages()) {
-    $itempage = $accounts->getNextPage();
-    echo "\nPage: " . $accounts->page . ' of ' . $accounts->getPageCount() . "\n";
-    foreach($itempage as $account) {
-        echo $account->username . "\n";
-        echo $account->type . "\n";
-    }
+include_once '../vendor/autoload.php';
+$client = new \Contactually\Client($apikey);
+$accounts = $client->get('accounts.json');
+$accountList = $accounts['accounts'];
+foreach($accountList as $account) {
+    echo $account['username'] . "\n";
+    echo $account['type'] . "\n";
 }
