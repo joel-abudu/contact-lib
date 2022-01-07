e812e000b54cfd95fc360ea414c54eb5f3fdbe82
Joel <joel.abudu@devit-team.com>
2022-01-07 14:56:19 +0000

    wired the contacts class

--- examples/contacts-index.php
+++ examples/contacts-index.php
@@ -1,17 +1,8 @@
 <?php
 include_once '../creds.php';
-include_once '../Services/Contactually.php';
-$client = new Services_Contactually(array('api_key' => $apiKey));
-$contacts = $client->contacts->index($page = 1, $limit = 10);
-echo "\nDisplaying {$contacts->count} records per page:\n";
-echo "\nPage: " . $contacts->page . ' of ' . $contacts->getPageCount() . "\n";
+include_once '../vendor/autoload.php';
+$client = new \Contactually\Client($apikey);
+$contacts = $client->contacts->index();
 foreach($contacts as $contact) {
     echo $contact->first_name . " " . $contact->last_name . "\n";
-}
-while ($contacts->hasMorePages()) {
-    $itempage = $contacts->getNextPage();
-    echo "\nPage: " . $contacts->page . ' of ' . $contacts->getPageCount() . "\n";
-    foreach($itempage as $contact) {
-        echo $contact->first_name . " " . $contact->last_name . "\n";
-    }
 }
