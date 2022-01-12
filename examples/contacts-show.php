d79fa335434ac40345d28c2a2bca4b972bdcfeab
Joel <joel.abudu@devit-team.com>
2022-01-12 04:24:40 +0000

    wired contacts->show()

--- examples/contacts-show.php
+++ examples/contacts-show.php
@@ -1,7 +1,6 @@
 <?php
 include_once '../creds.php';
-include_once '../Services/Contactually.php';
-$client = new Services_Contactually(array('api_key' => $apiKey));
-$contact = new Services_Contactually_Contact($client);
-$result = $contact->show(14184603);
-print_r($result);
+include_once '../vendor/autoload.php';
+$client = new \Contactually\Client($apikey);
+$contact = $client->contacts->show(14184603);
+print_r($contact);
