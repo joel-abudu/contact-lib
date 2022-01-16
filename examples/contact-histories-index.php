18bbd9385f83b40e929ba076fc1ddd150251f2b2
Joel <joel.abudu@devit-team.com>
2022-01-16 18:36:43 +0000

    wired the contact histories->index()

--- examples/contact-histories-index.php
+++ examples/contact-histories-index.php
@@ -1,19 +1,9 @@
 <?php
 include_once '../creds.php';
-include_once '../Services/Contactually.php';
-$client = new Services_Contactually(array('api_key' => $apiKey));
-$contact_histories = $client->contact_histories->index($page = 1, $limit = 10);
-echo "\nDisplaying {$contact_histories->count} records per page:\n";
-echo "\nPage: " . $contact_histories->page . ' of ' . $contact_histories->getPageCount() . "\n";
-foreach($contact_histories as $contact_history) {
-    echo $contact_history->email . "\n";
-    echo $contact_history->subject . "\n";
-}
-while ($contact_histories->hasMorePages()) {
-    $itempage = $contact_histories->getNextPage();
-    echo "\n\nPage: " . $contact_histories->page . ' of ' . $contact_histories->getPageCount() . "\n";
-    foreach($itempage as $contact_history) {
-        echo $contact_history->email . "\n";
-        echo $contact_history->subject . "\n";
-    }
+include_once '../vendor/autoload.php';
+$client = new \Contactually\Client($apikey);
+$histories = $client->contact_histories->index();
+foreach($histories as $history) {
+    echo $history->email . "\n";
+    echo $history->subject . "\n";
 }
---
+++ src/Contactually/ContactHistories.php
@@ -0,0 +1,7 @@
+<?php
+namespace Contactually;
+class ContactHistories extends \Contactually\Common\Resource
+{
+    protected $resource = 'contact_histories';
+    protected $dataname = 'email_histories';
+}
