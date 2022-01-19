03443a6d57fa90e74737a6b0dcd7f3f7fda6ce99
Joel <joel.abudu@devit-team.com>
2022-01-19 14:13:38 +0000

    killed the unnecessary example

--- examples/authenticate.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once '../creds.php';
-include_once '../Services/Contactually.php';
-$client = new Services_Contactually(array('api_key' => $apiKey));
-$buckets = $client->buckets->index();
-foreach($buckets as $bucket) {
-    echo $bucket->name . "\n";
-}
