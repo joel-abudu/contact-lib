04eee5d9f832537434e8c2de13c06728a5b8640f
Joel <joel.abudu@devit-team.com>
2022-01-30 18:58:01 +0000

    implemented an advanced index()

--- src/Contactually/Contacts.php
+++ src/Contactually/Contacts.php
@@ -4,4 +4,10 @@ class Contacts extends \Contactually\Common\Resource
 {
     protected $resource = 'contacts';
     protected $dataname = 'contacts';
+    public function search($term, $parameters = array())
+    {
+        $parameters['term'] = $term;
+        $this->resource = 'contacts/search';
+        return parent::index($parameters);
+    }
 }
