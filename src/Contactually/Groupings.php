aaf8cb9e0ae1a15a5523c27e2a673353a6e5be24
Joel <joel.abudu@devit-team.com>
2022-02-11 18:43:04 +0000

    wired grouping statistics()

--- src/Contactually/Groupings.php
+++ src/Contactually/Groupings.php
@@ -4,4 +4,9 @@ class Groupings extends \Contactually\Common\Resource
 {
     protected $resource = 'groupings';
     protected $dataname = 'groupings';
+    public function statistics($id)
+    {
+        $results = $this->client->get($this->resource . '/' . $id . '/statistics.json');
+        return json_decode(json_encode($results));
+    }
 }
