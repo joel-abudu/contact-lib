09f3614e9423ad607e41b786e62066c0de7832db
Joel <joel.abudu@devit-team.com>
2022-02-16 14:33:21 +0000

    wired statistics on the contacts resource

--- src/Contactually/Contacts.php
+++ src/Contactually/Contacts.php
@@ -10,4 +10,9 @@ class Contacts extends \Contactually\Common\Resource
         $this->resource = 'contacts/search';
         return parent::index($parameters);
     }
+    public function statistics($id)
+    {
+        $results = $this->client->get($this->resource . '/' . $id . '/statistics.json');
+        return json_decode(json_encode($results));
+    }
 }
