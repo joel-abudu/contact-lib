ff4626ae6f5c083b8e0d81199b4539a50aed2c2f
Joel <joel.abudu@devit-team.com>
2022-02-21 02:05:07 +0000

    wiring the post functionality

--- src/Contactually/Client.php
+++ src/Contactually/Client.php
@@ -35,6 +35,14 @@ class Client
     }
     public function post($uri, $params = array())
     {
+        $request = $this->client->post($uri, array(), '', array('exceptions' => false));
+        foreach($params as $key => $value) {
+            $request->setPostField($key, $value);
+        }
+        $this->response = $request->send();
+        $this->statusCode = $this->response->getStatusCode();
+        $this->detail = $this->response->json();
+        return $this->response->isSuccessful();
     }
     public function delete($uri)
     {
