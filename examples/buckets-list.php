aad42b6b53b9aa1e8799388199cbdd50f44cdd43
Joel <joel.abudu@devit-team.com>
2022-02-23 21:37:55 +0000

    removing the deprecated test code

--- examples/buckets-list.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once '../creds.php';
-include_once '../Services/Contactually.php';
-$client = new Services_Contactually(array('api_key' => $apiKey));
-$buckets = $client->buckets->list();
-foreach($buckets as $bucket) {
-    echo $bucket->title . "\n";
-}
