eb5789bbc5b032e92eb7879a28036b6a2cd51991
Joel <joel.abudu@devit-team.com>
2022-03-01 15:07:19 +0000

    wired the followups to work as expected

--- examples/followups-index.php
+++ examples/followups-index.php
@@ -4,5 +4,6 @@ include_once '../vendor/autoload.php';
 $client = new \Contactually\Client($apikey);
 $followups = $client->followups->index();
 foreach($followups as $followup) {
-    print_r($followup);
+    echo $followup->title . "\n";
+    echo $followup->due_date . "\n";
 }
