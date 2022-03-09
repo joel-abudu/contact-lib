7278bb1019a3b47d4ac748584f3e10abb797275c
Joel <joel.abudu@devit-team.com>
2022-03-09 13:12:03 +0000

    removing the old library structure

--- Services/Contactually/ContactHistories.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Services_Contactually_ContactHistories extends Services_Contactually_Resources_List
-    implements Services_Contactually_Interfaces_Index
-{
-    protected $_data = 'email_histories';
-    protected $_class = 'Services_Contactually_ContactHistory';
-    protected $_index_uri = 'contact_histories.json';
-}
