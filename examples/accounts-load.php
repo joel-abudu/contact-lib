b5dae133be19eb3bd277a3c3f9d9a38a99522e32
Joel <joel.abudu@devit-team.com>
2022-03-16 12:30:55 +0000

    wired to a correct account

--- examples/accounts-load.php
+++ examples/accounts-load.php
@@ -2,5 +2,5 @@
 include_once '../creds.php';
 include_once '../vendor/autoload.php';
 $client = new \Contactually\Client($apikey);
-$account = $client->accounts->load(105747);
+$account = $client->accounts->load(172799);
 print_r($account);
