143e3dd974c4490dbba1de4d55c435eb24e601bc
Joel <joel.abudu@devit-team.com>
2022-03-22 14:58:49 +0000

    creating some items

--- examples/notes-create.php
+++ examples/notes-create.php
@@ -4,7 +4,7 @@ include_once '../vendor/autoload.php';
 $client = new \Contactually\Client($apikey);
 $params = array(
     'body' => 'This is the body of my note',
-    'contact_id' => 17471066,
+    'contact_id' => 13194473,
     );
 $result = $client->notes->create($params);
 echo $result;
