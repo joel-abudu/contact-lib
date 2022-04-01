42e8a5be472c97c835c2d059a56d9a6bd567f5f7
Joel <joel.abudu@devit-team.com>
2022-04-01 05:19:29 +0000

    wiring a bit more of the API

---
+++ examples/webhooks-index.php
@@ -0,0 +1,8 @@
+<?php
+include_once '../creds.php';
+include_once '../vendor/autoload.php';
+$client = new \Contactually\Client($apikey);
+$webhooks = $client->webhooks->index();
+foreach($webhooks as $webhook) {
+    print_r($webhook);
+}
