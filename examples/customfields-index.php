5b5078d82ca88859ff1730395790c3f922d0952e
Joel <joel.abudu@devit-team.com>
2022-04-02 15:06:24 +0000

    wiring more of the examples

--- examples/customfields-index.php
+++ examples/customfields-index.php
@@ -4,5 +4,6 @@ include_once '../vendor/autoload.php';
 $client = new \Contactually\Client($apikey);
 $fields = $client->customfields->index();
 foreach($fields as $field) {
+    print_r($field);
     echo $field->field_label . "\n";
 }
---
+++ examples/customfields-load.php
@@ -0,0 +1,6 @@
+<?php
+include_once '../creds.php';
+include_once '../vendor/autoload.php';
+$client = new \Contactually\Client($apikey);
+$customfields = $client->customfields->load(62292);
+print_r($customfields);
