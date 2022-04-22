c5fc0bfb0ff5aca31f8ca71318f172df5626276a
Joel <joel.abudu@devit-team.com>
2022-04-22 10:43:45 +0000

    wired examples of ignore and snooze

--- examples/contacts-snooze.php
+++ examples/contacts-snooze.php
@@ -3,5 +3,5 @@ include_once '../creds.php';
 include_once '../vendor/autoload.php';
 $client = new \Contactually\Client($apikey);
 $contact = $client->contacts->load(46955899);
-$result = $contact->ignore();
+$result = $contact->snooze();
 print_r($result);
